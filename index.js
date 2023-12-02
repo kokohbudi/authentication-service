2cadf53748ba658408c752820b4f1994965237db
Kokoh Budi Romansyah <kokohbudi@gmail.com>
2023-12-02 10:49:45 +0700

    deploy

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+// const { Logging } = require('@google-cloud/logging');
+const TelegramBot = require("node-telegram-bot-api");
+
+
+const app = express();
+// const logging = new Logging({
+//   projectId: 'panda-manda-services',
+// });
+
+const token = '6313749780:AAF1Pv1CThOGHjFoakUcVXIppY-xmE7RVZ8';
+const bot = new TelegramBot(token);
+
+
+
+app.post('/', (req, res) => {
+  if (req.headers['x-telegram-bot-api-secret-token'] !== 'sibebekgantengbangetehe') {
+    res.status(401).send('Unauthorized, GO OUT!!');
+    return;
+  }
+  // const message = `Received POST request with body: ${JSON.stringify(req.body)}`;
+  // console.log(message);
+  const sender=req.body.message.from.id;
+  const chat=req.body.message.text;
+  bot.sendMessage(sender, chat);
+  res.send('OK');
+  return;
+});
+
+exports.webhook = app;
